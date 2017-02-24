82bb6cb1f67081d8fa15c720fe5dd11b8a8c53ec
proxymus <michael.illgen@googlemail.com>
2017-02-24 21:20:11 +0100

    Added missing comma

--- lang/en.js
+++ lang/en.js
@@ -1,5 +1,5 @@
 CKEDITOR.plugins.setLang("note","en",{
-    	title: 'Insert a note'
+		title: 'Insert a note',
 		selectOption: 'Which kind of note you would like to insert?',
 		content: 'Note content:'
 });
