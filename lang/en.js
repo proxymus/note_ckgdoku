7666bb49e1c7e510af047f77c408862459806ef6
proxymus <michael.illgen@googlemail.com>
2017-08-17 08:25:43 +0200

    Update en.js

--- lang/en.js
+++ lang/en.js
@@ -1,5 +1,9 @@
 CKEDITOR.plugins.setLang("note","en",{
 		title: 'Insert a note',
 		selectOption: 'Which kind of note you would like to insert?',
-		content: 'Note content:'
+		content: 'Note content:',
+                basic: 'Note',
+                important: 'Important',
+                tip: 'Tip',
+                warning: 'Warning',
 });
